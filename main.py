5ca7de8930f853e2fe5cf19ae2cb2f4fd0310d62
Hannanah Mobashir <hanana14@yahoo.com>
2017-08-20 15:02:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from linkedlist import Element
 from linkedlist import LinkedList
 
-element1 = Element(2)
-element2 = Element(2)
+# create a instances of Element
+element1 = Element(5)
+element2 = Element(0)
 element3 = Element(2)
-element4 = Element(2)
+element4 = Element(8)
 element5 = Element(7)
 element6 = Element(9)
 element7 = Element(2)
 
+# create an instance of LinkedList with head = element1
 list1 = LinkedList(element1)
+
+# add a few elements to the LinkedList instance
 list1.add_item(element2)
 list1.add_item(element3)
 list1.add_item(element4)
 list1.add_item(element5)
-print list1.show_all()
-list1.remove_item(2)
-print list1.show_all()
 list1.add_item(element6)
-#list1.add_item(element7)
+list1.add_item(element7)
+
+# print values to confirm that items were added
 print list1.show_all()
+
+# remove a few values from the list
 list1.remove_item(2)
+list1.remove_item(5)
+
+# print values to confirm that items were added
 print list1.show_all()
+
+# print position of the element at the position = 2
+print list1.get_position(2)
