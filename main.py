3e434c1c61600184adb5c64b9d3a5e151c1df719
Hannanah Mobashir <hanana14@yahoo.com>
2017-08-20 16:01:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,28 +24,30 @@ list1.add_item(element5)
 # print values to confirm that items were added
 print list1.show_all()
 
-# remove a few values from the list
+# remove a few values from the list and confirm removal
 list1.remove_item(2)
 list1.remove_item(5)
-
-# print values to confirm that items were added
 print list1.show_all()
 
 # print position of the element at the position = 2
 print list1.get_position(2)
 
-# insert items at a given position
+# insert items at a given position and confirm addition
 list1.insert_item(element6, 1)
-# print values to confirm that items were added
 print list1.show_all()
 
-# insert items at a given position
+# insert items at a given position and confirm addition
 list1.insert_item(element7, 4)
-# print values to confirm that items were added
 print list1.show_all()
 
-# insert items at a given position
+# insert items at a given position and confirm addition
 list1.insert_item(element8, 2)
-# print values to confirm that items were added
 print list1.show_all()
 
+# remove item from given position and confirm deletion
+list1.remove_item_from(5)
+print list1.show_all()
+
+# remove item from given position and confirm deletion
+list1.remove_item_from(1)
+print list1.show_all()
