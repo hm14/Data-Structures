77f01afc2c6dc568d03a3c930313c4367184344c
Hannanah Mobashir <hanana14@yahoo.com>
2017-08-21 15:25:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,24 +30,45 @@ list1.remove_item(5)
 print list1.show_all()
 
 # print position of the element at the position = 2
+print 'getting position of 2nd element in linked list'
 print list1.get_position(2)
 
 # insert items at a given position and confirm addition
 list1.insert_item(element6, 1)
+print 'inserting new element at 1st position'
 print list1.show_all()
 
 # insert items at a given position and confirm addition
 list1.insert_item(element7, 4)
+print 'inserting new element at 4th position'
 print list1.show_all()
 
 # insert items at a given position and confirm addition
 list1.insert_item(element8, 2)
+print 'inserting new element at 2nd position'
 print list1.show_all()
 
 # remove item from given position and confirm deletion
 list1.remove_item_from(5)
+print 'removing 5th item:'
 print list1.show_all()
 
 # remove item from given position and confirm deletion
 list1.remove_item_from(1)
+print 'removing 1st item:'
+print list1.show_all()
+
+# delete given value and confirm deletion
+list1.delete_item(0)
+print 'deleting 0:'
+print list1.show_all()
+
+# delete given value and confirm deletion
+list1.delete_item(18)
+print 'deleting 18:'
+print list1.show_all()
+
+# delete given value and confirm deletion
+list1.delete_item(7)
+print 'deleting 7:'
 print list1.show_all()
