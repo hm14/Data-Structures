067909874e5336193b162bcf6aec2a874166ca6b
Hannanah Mobashir <hanana14@yahoo.com>
2017-08-21 23:32:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from linkedlist import Element
 from linkedlist import LinkedList
+from stack import Stack
 
 # create a instances of Element
 element1 = Element(5)
@@ -11,6 +12,8 @@ element6 = Element(9)
 element7 = Element(2)
 element8 = Element(18)
 element9 = Element(19)
+element10 = Element(10)
+element11 = Element(82)
 
 # create an instance of LinkedList with head = element1
 list1 = LinkedList(element1)
@@ -22,11 +25,13 @@ list1.add_item(element4)
 list1.add_item(element5)
 
 # print values to confirm that items were added
+print 'adding multiple elements to linked list'
 print list1.show_all()
 
 # remove a few values from the list and confirm removal
 list1.remove_item(2)
 list1.remove_item(5)
+print 'removing multiple elements from linked list'
 print list1.show_all()
 
 # print position of the element at the position = 2
@@ -41,11 +46,6 @@ print list1.show_all()
 # insert items at a given position and confirm addition
 list1.insert_item(element7, 4)
 print 'inserting new element at 4th position'
-print list1.show_all()
-
-# insert items at a given position and confirm addition
-list1.insert_item(element8, 2)
-print 'inserting new element at 2nd position'
 print list1.show_all()
 
 # remove item from given position and confirm deletion
@@ -64,11 +64,26 @@ print 'deleting 0:'
 print list1.show_all()
 
 # delete given value and confirm deletion
-list1.delete_item(18)
-print 'deleting 18:'
+list1.delete_item(8)
+print 'deleting 8:'
 print list1.show_all()
 
-# delete given value and confirm deletion
-list1.delete_item(7)
-print 'deleting 7:'
-print list1.show_all()
+# create an instance of LinkedList with head = element1
+print 'creating a new stack'
+stack = Stack(element1)
+
+# add a few elements to the LinkedList instance
+stack.push(element8)
+stack.push(element9)
+stack.push(element10)
+stack.push(element11)
+
+#print values to confirm that elements were added to stack
+print 'adding multiple items to stack'
+print stack.show_all()
+
+print 'removing multiple items from stack'
+stack.pop()
+stack.pop()
+stack.pop()
+print stack.show_all()
